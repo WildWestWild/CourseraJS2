3562203d713964d323b2dc1f6fdd2b77a68eedd5
Maxim <mainmaxxx@gmail.com>
2019-04-07 00:40:35 +0300

    Finish script but no check

--- quest_3/public/script.js
+++ quest_3/public/script.js
@@ -7,9 +7,15 @@ function checkForRegExp(str, regExp) {
     return false;
   }
 }
-
+function getForm(object){
+  let form = document.getElementById(object.formId);
+    form.formValidClass = object.formValidClass;
+    form.formInvalidClass = object.formInvalidClass;
+    form.inputErrorClass = object.inputErrorClass;
+  return form;
+}
 function validateForm(object) {
-  object.checkInput = function(element) {
+  var checkInput = function checkInputFunction(element) {
     // Проверка input
     console.log(element);
     if (element.tagName !== "INPUT") {
@@ -19,7 +25,7 @@ function validateForm(object) {
       case "name":
         if (checkForRegExp(element.value, /[a-zа-я]/g)) {
           console.log("profile-name is true");
-          return this;
+          return true;
         }
         break;
       case "age":
@@ -29,19 +35,19 @@ function validateForm(object) {
           Number(element.value) <= Number(element.dataset.validatorMax)
         ) {
           console.log("profile-age is true");
-          return this;
+          return true;
         }
         break;
       case "phone":
         if (checkForRegExp(element.value, element.dataset.validatorPattern)) {
           console.log("profile-phone is true");
-          return this;
+          return true;
         }
         break;
       case "number":
         if (checkForRegExp(element.value, /\d+/)) {
           console.log("profile-name is true");
-          return this;
+          return true;
         }
         break;
       default:
@@ -49,58 +55,59 @@ function validateForm(object) {
     }
     if (element.value == "" && element.name != "name") {
       console.log(element.value, "is true");
-      return this;
-    }
-    element.classList.add(this.inputErrorClass);
-    return this; // Есть ошибки
-  };
-  object.deleteClass = function(event) {
-    // Если навели фокус на элемент удалим класс с ошибкой
-    let element = event.target;
-    if (element.classList.contains(this.inputErrorClass)) {
-      element.classList.remove(this.inputErrorClass);
-      console.log(this.inputErrorClass);
-      console.log(" is deleted");
       return true;
     }
-    return true;
+    element.classList.add(this.inputErrorClass);
+    return false; // Есть ошибки
   };
-  object.checkSubmit = function(event) {
+  var deleteClass = function(event, errorClass) {
+    // Если навели фокус на элемент удалим класс с ошибкой
+    let element = event.target;
+    if (errorClass == undefined) {
+      errorClass = this.inputErrorClass;
+    }
+    if (element.classList.contains(errorClass)) {
+      element.classList.remove(errorClass);
+      console.log(this);
+      console.log(" is deleted");
+    }
+  };
+  var checkSubmit = function(event) {
     //Не оправляем форму, проверяем))
     event.preventDefault();
     //Проверить все элементы
-    if (this.goToAllElements(event)) {
+    if (goToAllElements(this)) {
       /*
   Если все хорошо - 
   1. Удалить (если есть) formInvalidClass
   2. Добавить formValidClass
   */
-
-      event.classList.add(this.formValidClass);
+      deleteClass(event, this.formInvalidClass);
+      this.classList.add(this.formValidClass);
     } else {
       //Если есть повесить класс о неправильной работе формы form__error-msg
       deleteClass(event, this.formValidClass);
-      event.classList.add(this.formInvalidClass);
+      this.classList.add(this.formInvalidClass);
     }
   };
-  object.goToAllElements = function(form) {
-    let elements = form.target.getElementsByTagName("input");
+  var goToAllElements = function(form) {
+    let elements = form.getElementsByTagName("input");
     for (let i = 0, len = elements.length; i < len; i++) {
       if (elements[i].hasAttribute("name")) {
-        if (!this.checkInput(elements[i])) {
+        if (!checkInput.call(form, elements[i])) {
           return false;
         }
       }
     }
     return true;
   };
-  function createEventsForm(form, object) {
-    form.addEventListener("focus", deleteClass, true);
+
+  function createEventsForm(form) {
+    form.addEventListener("focus",deleteClass, true);
     form.addEventListener("blur", checkInput, true);
     form.addEventListener("submit", checkSubmit);
   };
   // Получим данные с формы
-  var form = document.getElementById(object.formId);
-  console.log(form);
-  createEventsForm(form, object);
+  let form = getForm(object);
+  createEventsForm(form);
 }
